5f9baf3a766da9caed4614186185070bf6070bb7
Dennis Smuda <smuda.dennis@gmail.com>
2022-07-12 22:36:06 +0200

    test: test for correct content on homepage

--- tests/home-page.spec.ts
+++ tests/home-page.spec.ts
@@ -26,3 +26,28 @@ test('index page shows a working cta button for the blog', async ({ page }) => {
 	await blogCta.click()
 	await expect(page).toHaveTitle(/Writing/)
 })
+
+test('index page shows my three lastest projects', async ({ page }) => {
+	await page.goto('/')
+
+	const cvEntries = await page.locator('a.project')
+	await expect(cvEntries).toHaveCount(3)
+
+	const firstEntry = cvEntries.nth(0)
+	await expect(firstEntry).toHaveAttribute('target', '_blank')
+})
+
+test('index page shows my two lastest posts', async ({ page }) => {
+	await page.goto('/')
+
+	const cvEntries = await page.locator('a.latest-post')
+	await expect(cvEntries).toHaveCount(2)
+
+	const firstEntry = cvEntries.first()
+	await expect(firstEntry).not.toHaveAttribute('target', '_blank')
+	await expect(firstEntry).toHaveAttribute('href', /blog/)
+
+	const secondEntry = cvEntries.last()
+	await expect(secondEntry).not.toHaveAttribute('target', '_blank')
+	await expect(secondEntry).toHaveAttribute('href', /blog/)
+})
