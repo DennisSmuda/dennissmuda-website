491bd6a0f4be1d391461701a3efd31e4bd926f90
Dennis Smuda <smuda.dennis@gmail.com>
2022-07-13 00:06:27 +0200

    update

--- playwright.config.js
+++ playwright.config.js
@@ -3,10 +3,10 @@
 const config = {
 	webServer: {
 		// command: 'npm run dev',
-		command: 'npm run build && npm run preview',
-		port: 3000
-		// reuseExistingServer: true
-		// port: 4173
+		// command: 'npm run build && npm run preview',
+		// port: 3000
+		reuseExistingServer: true,
+		port: 4173
 	}
 }
 
