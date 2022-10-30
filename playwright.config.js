7eb5005be4d69179fc8347898cf165ae280a3b4f
Dennis Smuda <smuda.dennis@gmail.com>
2022-10-30 22:53:00 +0100

    test confusion

--- playwright.config.js
+++ playwright.config.js
@@ -1,13 +1,13 @@
 const config = {
 	webServer: {
 		// Use this for dev:
-		// command: 'npm run dev',
-		// port: 5173,
-		// reuseExistingServer: true
+		command: 'npm run dev',
+		port: 5173,
+		reuseExistingServer: true
 
 		// this for CI/Prod
-		command: 'npm run preview',
-		port: 4173
+		// command: 'npm run preview',
+		// port: 4173
 	}
 }
 
