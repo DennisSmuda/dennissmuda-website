77ae1b604fa8fefbe50c04b52481c936dbf6db1e
Dennis Smuda <smuda.dennis@gmail.com>
2022-10-30 21:53:30 +0100

    fix: lol

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,12 @@
-const isDev = process.env.MODE === 'development' || true
+// const isDev = process.env.MODE === 'development' ? true : false
 
 const config = {
 	webServer: {
-		command: isDev ? 'npm run dev' : 'npm run preview',
-		port: isDev ? 5173 : 4173,
-		reuseExistingServer: isDev ? true : false
+		// command: isDev ? 'npm run dev' : 'npm run preview',
+		// port: isDev ? 5173 : 4173,
+		// reuseExistingServer: isDev ? true : false
+		command: 'npm run preview',
+		port: 4173
 	}
 }
 
