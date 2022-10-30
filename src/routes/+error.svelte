ce8034e6d32ffd6a1940fde414fb19b930e06b0b
Dennis Smuda <smuda.dennis@gmail.com>
2022-10-30 17:34:25 +0100

    feat: custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Header from '$lib/components/header.svelte'
+	import { page } from '$app/stores'
+</script>
+
+<Header title={$page.status.toString()} subtitle={$page.error?.message || ''} />
+
+<section class="container lg:max-w-4xl mx-auto px-8">
+	<div class="ds-prose mt-20">
+		<p>
+			<span>something seems off... there is no page for:</span>
+			<code>
+				{$page.url}
+			</code>
+		</p>
+
+		<a class="marker-link mt-4" href="/">go back 🧐</a>
+	</div>
+</section>
