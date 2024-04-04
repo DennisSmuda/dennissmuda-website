ab1dc4ded3b215bba5851c751b947ec976d0b445
Dennis Smuda <smuda.dennis@gmail.com>
2024-04-04 21:39:40 +0200

    test: init playwright tests!

--- tests/home-page.spec.js
+++ tests/home-page.spec.js
@@ -1,51 +1,50 @@
 import { expect, test } from '@playwright/test'
-import { topProjects } from '../src/lib/data/projects.js'
+// import { topProjects } from '../src/lib/data/projects.js'
 
 test.describe('page: index', () => {
-	test.beforeEach(async ({ page }) => {
-		await page.goto('/')
-	})
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/')
+  })
 
-	test('has expected hero & title', async ({ page }) => {
-		await expect(page).toHaveTitle(/Dennis Smuda/)
+  test('has expected hero & title', async ({ page }) => {
+    await expect(page).toHaveTitle(/Dennis Smuda/)
 
-		page.textContent
-		expect(await page.textContent('p.intro__paragraph')).toContain(
-			'web developer'
-		)
-	})
+    expect(await page.textContent('p.intro__paragraph')).toContain(
+      'web developer',
+    )
+  })
 
-	test('shows all 3 important headlines', async ({ page }) => {
-		const headlines = await page.locator('h2.rotated')
-		await expect(headlines).toHaveCount(3)
-	})
+  test('shows all 3 important headlines', async ({ page }) => {
+    const headlines = await page.locator('h2.rotated')
+    await expect(headlines).toHaveCount(3)
+  })
 
-	test('shows a working cta button for the blog', async ({ page }) => {
-		const blogCta = await page.locator('aside .marker-link')
-		await expect(blogCta).toBeDefined
+  test('shows a working cta button for the blog', async ({ page }) => {
+    const blogCta = await page.locator('aside .marker-link')
+    await expect(blogCta).toBeDefined
 
-		await blogCta.click()
-		await expect(page).toHaveTitle(/Writing/)
-	})
+    await blogCta.click()
+    await expect(page).toHaveTitle(/Writing/)
+  })
 
-	test('shows my three latest projects', async ({ page }) => {
-		const projects = await page.locator('a.project')
-		await expect(projects).toHaveCount(topProjects.length)
+  // test('shows my three latest projects', async ({ page }) => {
+  //   const projects = await page.locator('a.project')
+  //   await expect(projects).toHaveCount(topProjects.length)
 
-		const firstEntry = projects.nth(0)
-		await expect(firstEntry).toHaveAttribute('target', '_blank')
-	})
+  //   const firstEntry = projects.nth(0)
+  //   await expect(firstEntry).toHaveAttribute('target', '_blank')
+  // })
 
-	test('shows my latest posts', async ({ page }) => {
-		const cvEntries = await page.locator('a.latest-post')
-		await expect(cvEntries).toHaveCount(4)
+  test('shows my latest posts', async ({ page }) => {
+    const cvEntries = await page.locator('a.latest-post')
+    await expect(cvEntries).toHaveCount(4)
 
-		const firstEntry = cvEntries.first()
-		await expect(firstEntry).not.toHaveAttribute('target', '_blank')
-		await expect(firstEntry).toHaveAttribute('href', /blog/)
+    const firstEntry = cvEntries.first()
+    await expect(firstEntry).not.toHaveAttribute('target', '_blank')
+    await expect(firstEntry).toHaveAttribute('href', /blog/)
 
-		const secondEntry = cvEntries.last()
-		await expect(secondEntry).not.toHaveAttribute('target', '_blank')
-		await expect(secondEntry).toHaveAttribute('href', /blog/)
-	})
+    const secondEntry = cvEntries.last()
+    await expect(secondEntry).not.toHaveAttribute('target', '_blank')
+    await expect(secondEntry).toHaveAttribute('href', /blog/)
+  })
 })
--- tests/navbar.spec.js
+++ tests/navbar.spec.js
@@ -1,37 +1,37 @@
 import { expect, test } from '@playwright/test'
 
 test.describe('component: navbar', () => {
-	test.beforeEach(async ({ page }) => {
-		await page.goto('/')
-	})
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/')
+  })
 
-	test('shows logo', async ({ page }) => {
-		const logo = await page.locator('header.header .logo')
-		await expect(logo).toContainText('ds')
-	})
+  test('shows logo', async ({ page }) => {
+    const logo = await page.locator('header.header .logo')
+    await expect(logo).toContainText('ds')
+  })
 
-	test('shows 3 nav links', async ({ page }) => {
-		const navLinks = await page.locator('.header__nav > a')
-		await expect(navLinks).toHaveCount(3)
-	})
+  test('shows 3 nav links', async ({ page }) => {
+    const navLinks = await page.locator('.header__nav > a')
+    await expect(navLinks).toHaveCount(3)
+  })
 
-	test('can toggle color theme', async ({ page }) => {
-		const themeButton = await page.locator('#theme-toggle')
-		const html = await page.locator('html')
+  test('can toggle color theme', async ({ page }) => {
+    const themeButton = await page.locator('#theme-toggle')
+    const html = await page.locator('html')
 
-		await page.waitForTimeout(1500)
-		await page.screenshot({ path: 'screenshot.png', fullPage: true })
+    await page.waitForTimeout(1500)
+    await page.screenshot({ path: 'screenshot.png', fullPage: true })
 
-		await themeButton.click()
+    await themeButton.click()
 
-		await expect(html).toHaveClass('dark')
+    await expect(html).toHaveClass('dark')
 
-		await page.reload()
+    await page.reload()
 
-		await page.waitForTimeout(1500)
-		await page.screenshot({ path: 'screenshot-dark.png', fullPage: true })
+    await page.waitForTimeout(1500)
+    await page.screenshot({ path: 'screenshot-dark.png', fullPage: true })
 
-		await themeButton.click()
-		await expect(html).not.toHaveClass('dark')
-	})
+    await themeButton.click()
+    await expect(html).not.toHaveClass('dark')
+  })
 })
