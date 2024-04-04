ca2262d660671f98fd54a785603351c51e426f42
Dennis Smuda <smuda.dennis@gmail.com>
2024-04-04 21:00:45 +0200

    chore: delete svelte project

--- src/routes/blog/+page.ts
+++
@@ -1,8 +0,0 @@
-import type { PageLoad } from './$types'
-
-export const load: PageLoad = async ({ fetch }) => {
-	const res = await fetch(`/api/blog`)
-	const data = await res.json()
-
-	return { posts: data.posts }
-}
