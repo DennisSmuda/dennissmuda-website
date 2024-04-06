92b2885a80b78b100198d94d5ca0285492e1f0de
Dennis Smuda <smuda.dennis@gmail.com>
2024-04-06 17:14:56 +0200

    feat: server route gets projects only!

--- features/home/FeaturedProjects.vue
+++ features/home/FeaturedProjects.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const { data, error, pending } = useFetch('/api/projects')
+const { data, error, pending } = useFetch('/api/projects?limit=30')
 </script>
 
 <template>
@@ -8,7 +8,9 @@ const { data, error, pending } = useFetch('/api/projects')
   <span v-if="pending">Loading...</span>
   <template v-for="project in data" v-else-if="data" :key="project._id">
     <Project
-      :title="project.title" :subtitle="project.subtitle" :description="project.description"
+      :title="project.title"
+      :subtitle="project.subtitle"
+      :description="project.description"
       :url="project.url"
     />
     <!-- {{project }} -->
--- server/api/projects.ts
+++ server/api/projects.ts
@@ -2,9 +2,10 @@ import { serverQueryContent } from '#content/server'
 
 export default defineEventHandler(async (event) => {
   const query = getQuery(event)
+
   if (query.limit) {
     const limit = Number.parseInt(query.limit as string)
-    const content = await serverQueryContent(event).limit(limit).find()
+    const content = await serverQueryContent(event).limit(limit).where({ _dir: 'projects' }).find()
     return content
   }
   else {
