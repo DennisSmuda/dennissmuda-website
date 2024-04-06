f367b4f67a313308e15f82e4ec9789f1b585a06d
Dennis Smuda <smuda.dennis@gmail.com>
2024-04-06 19:57:07 +0200

    feat: work page - list projects

--- pages/work.vue
+++ pages/work.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
-const { data, pending } = await useAsyncData(
+const { data } = await useAsyncData(
   'projects',
   () => queryContent('projects').sort({ createdAt: 1 }).find(),
 )
-const isActive = useState('activeProjectId')
+// const isActive = useState('activeProjectId')
 </script>
 
 <template>
   <PageTitle title="work 🏁" subtitle="let's see some of my projects" />
 
-  <div class="h-screen container mx-auto p-8">
-    Projects
-    <template v-for="post in data" :key="post._id">
-      {{ post.title }}
+  <div class="container mx-auto p-8 mt-8">
+    <template v-for="project in data" :key="project._id">
+      <Project
+        :title="project.headline"
+        :subtitle="project.subline"
+        :description="project.description"
+        :url="project.url"
+      />
     </template>
   </div>
 </template>
