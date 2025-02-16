1341f512624ff30716fefd49ad6e17da9fe880e9
Dennis Smuda <smuda.dennis@gmail.com>
2025-02-17 00:30:46 +0100

    chore: add coverage reporter

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,11 @@ export default defineVitestConfig({
   test: {
     environment: 'nuxt',
     exclude: ['**/e2e/**', '**/node_modules/**'],
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ['text', 'json-summary', 'json'],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    },
   },
 })
