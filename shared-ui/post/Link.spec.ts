c6b08d981ddc19635a721de5d3cea0c21bfb4abf
Dennis Smuda <smuda.dennis@gmail.com>
2025-02-19 22:59:16 +0100

    feat: add tests for shared components

---
+++ shared-ui/post/Link.spec.ts
@@ -0,0 +1,52 @@
+import type { PostLinkProps } from './Link.vue'
+import { mount } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import Link from './Link.vue'
+
+const MOCK_POST = {
+  id: '1',
+  title: 'Title',
+  slug: 'slug',
+  createdAt: '2021-01-01',
+  description: 'Description',
+  subtitle: 'Subtitle',
+} as PostLinkProps
+
+function mountPostLink(props: PostLinkProps = MOCK_POST) {
+  return mount(Link, {
+    props,
+    global: {
+      stubs: {
+        NuxtLink: {
+          template: '<a><slot /></a>',
+        },
+      },
+    },
+  })
+}
+
+describe('shared: PostLink', () => {
+  it('should be defined', () => {
+    expect(Link).toBeDefined()
+  })
+
+  it('should render correctly', () => {
+    const wrapper = mountPostLink()
+    expect(wrapper.exists()).toBe(true)
+    wrapper.unmount()
+  })
+
+  it('should render the title', () => {
+    const wrapper = mountPostLink()
+    const title = wrapper.find('h3')
+    expect(title.text()).toBe(MOCK_POST.title)
+    wrapper.unmount()
+  })
+
+  it('should render the created date', () => {
+    const wrapper = mountPostLink()
+    const date = wrapper.find('span')
+    expect(date.text()).toBe(MOCK_POST.createdAt)
+    wrapper.unmount()
+  })
+})
---
+++ shared-ui/project/Project.spec.ts
@@ -0,0 +1,52 @@
+import { describe, expect, it } from 'vitest'
+import Project, { type ProjectProps } from './Project.vue'
+import { mount } from '@vue/test-utils'
+
+const MOCK_PROJECT = {
+  title: 'Title',
+  description: 'Description',
+  subtitle: 'Subtitle',
+  url: 'https://example.com',
+} as ProjectProps
+
+function mountProject(props: ProjectProps = MOCK_PROJECT) {
+  return mount(Project, {
+    props,
+  })
+}
+
+describe('shared: Project', () => {
+  it('should be defined', () => {
+    expect(Project).toBeDefined()
+  })
+
+  it('should render correctly', () => {
+    const wrapper = mountProject()
+    expect(wrapper.exists()).toBe(true)
+    wrapper.unmount()
+  })
+
+  it('should render the title', () => {
+    const wrapper = mountProject()
+    const title = wrapper.find('h2')
+    expect(title.text()).toBe(MOCK_PROJECT.title)
+    wrapper.unmount()
+  })
+
+  it('should render the description', () => {
+    const wrapper = mountProject()
+    const description = wrapper.findAll('p').at(1)
+    expect(description?.exists()).toBe(true)
+    expect(description?.text()).toBe(MOCK_PROJECT.description)
+    wrapper.unmount()
+  })
+
+  it('should render the subtitle', () => {
+    const wrapper = mountProject()
+    const description = wrapper.findAll('p').at(0)
+    expect(description?.exists()).toBe(true)
+    expect(description?.text()).toBe(MOCK_PROJECT.subtitle)
+    wrapper.unmount()
+  })
+
+})
