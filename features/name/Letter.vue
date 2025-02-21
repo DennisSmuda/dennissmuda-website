eee3b1273f8d8b208ce849a65280daec2ecc859d
Dennis Smuda <smuda.dennis@gmail.com>
2025-02-21 23:15:22 +0100

    feat: more animation (#56)
    * chore: reenable intro rotation
    
    * feat: better animations

--- features/name/Letter.vue
+++ features/name/Letter.vue
@@ -1,5 +1,5 @@
-<script setup lang="ts">
-import { Motion } from 'motion-v'
+<script lang="ts">
+import { delay } from 'motion-v'
 
 interface LetterProps {
   path: string
@@ -8,31 +8,68 @@ interface LetterProps {
   initialX?: number
   initialY?: number
 }
+</script>
+
+<script setup lang="ts">
 const props = withDefaults(defineProps<LetterProps>(), {
   initialX: 0,
   initialY: 100,
   animateColor: false,
 })
 
-const randomRotation = () => Math.floor(Math.random() * 11) - 5
+const opacity = ref(0)
+const positionY = ref(32)
+
+const ySpring = useSpring(32, {
+  stiffness: 180,
+  damping: 4,
+})
+
+const opacityMotion = useTransform(
+  ySpring,
+  [32, 24, 3, 0],
+  [0, 0, 1, 1],
+)
+
+useMotionValueEvent(ySpring, 'change', (latest) => {
+  positionY.value = latest
+})
+useMotionValueEvent(opacityMotion, 'change', (latest) => {
+  opacity.value = latest
+})
+
+delay(() => {
+  ySpring.set(0)
+}, props.delay / 1000)
+
+const rotation = ref(0)
+const rotationSpring = useSpring(0, {
+  stiffness: 180,
+  damping: 4,
+})
+const randomRotation = Math.floor(Math.random() * 11) - 5
+
+useMotionValueEvent(rotationSpring, 'change', (latest) => {
+  rotation.value = latest
+})
+
+function handleMouseOver() {
+  rotationSpring.set(randomRotation)
+}
 </script>
 
 <template>
-  <Motion
-    as="path"
+  <path
+    :d="path"
     class="fill-black dark:fill-white"
     :class="{ animate: props.animateColor }"
-    :hover="{ scale: 1.025 }"
-    :press="{
-      scale: 0.975,
-      rotate: randomRotation(),
-    }"
-    :transition="{
-      type: 'spring',
-      stiffness: 500,
-      damping: 10,
-    }"
-    :d="path"
+    :style="`
+      transform: translate3D(${0}%, ${positionY}%, 0px);
+      rotate: ${rotation}deg;
+      opacity: ${opacity};
+    `"
+    @mouseover="handleMouseOver"
+    @mouseleave="rotationSpring.set(0)"
   />
 </template>
 
