ac42aeea13162b0cf8b8ea45e0a801d215c325c8
Dennis Smuda <smuda.dennis@gmail.com>
2025-02-21 23:35:11 +0100

    feat: setup values in shared logo

--- shared-ui/navbar/Logo.vue
+++ shared-ui/navbar/Logo.vue
@@ -1,8 +1,59 @@
+<script lang="ts" setup>
+const initialX = 0
+const initialY = 0
+
+const springConfig = {
+  stiffness: 180,
+  damping: 4,
+}
+const xSpring = useSpring(initialX, springConfig)
+const ySpring = useSpring(initialY, springConfig)
+const parentRect = ref<DOMRect>()
+const elementRef = ref<HTMLElement>()
+
+const currentX = ref(initialX)
+const currentY = ref(initialY)
+
+onMounted(() => {
+  parentRect.value = elementRef.value?.getBoundingClientRect()
+})
+
+useMotionValueEvent(xSpring, 'change', (latest) => {
+  currentX.value = latest
+})
+useMotionValueEvent(ySpring, 'change', (latest) => {
+  currentY.value = latest
+})
+
+function handleMouseEnter(event: MouseEvent) {
+  const { screenX, screenY } = event
+  xSpring.set(screenX / window.innerWidth * 100)
+  ySpring.set(screenY / window.innerHeight * 100)
+  // const { width, height, left, top } = parentRect.value || { width: 0, height: 0, left: 0, top: 0 }
+  // const x = ((clientX - left) / width - 0.5) * 100
+  // const y = ((clientY - top) / height - 0.5) * 100
+  // xSpring.set(x)
+  // ySpring.set(y)
+}
+function handleMouseLeave() {
+  xSpring.set(initialX)
+  ySpring.set(initialY)
+}
+</script>
+
 <template>
-  <NuxtLink to="/" class="logo button">
-    ds
-    <span>.</span>
-  </NuxtLink>
+  <div
+    ref="elementRef"
+    @mouseenter="handleMouseEnter"
+    @mouseleave="handleMouseLeave"
+  >
+    <NuxtLink to="/" class="logo button relative">
+      ds
+      <span
+        :style="{ transform: `translate3D(${currentX}%, ${currentY}%, 0px)` }"
+      >.</span>
+    </NuxtLink>
+  </div>
 </template>
 
 <style scoped>
