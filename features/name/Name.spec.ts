6bfcebb28106968c3a216e960f2ac0de3fcc0278
Dennis Smuda <smuda.dennis@gmail.com>
2025-02-22 22:58:41 +0100

    chore: remove unused import

--- features/name/Name.spec.ts
+++ features/name/Name.spec.ts
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils'
-import { describe, expect, it, vi } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { checkA11y } from '~/test/utils'
 import Dot from './Dot.vue'
 import Name from './Name.vue'
