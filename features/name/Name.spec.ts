67433c96ebda47c2c9f22c316f47185754eaa5d3
Dennis Smuda <smuda.dennis@gmail.com>
2025-02-22 22:55:44 +0100

    test: more events testing for name component

--- features/name/Name.spec.ts
+++ features/name/Name.spec.ts
@@ -8,6 +8,8 @@ function mountName() {
   return mount(Name)
 }
 
+export const delay = (ms: number) => new Promise(r => setTimeout(r, ms))
+
 describe('feature: Name', () => {
   it('should be defined', () => {
     expect(Name).toBeDefined()
@@ -19,17 +21,33 @@ describe('feature: Name', () => {
     wrapper.unmount()
   })
 
-  it('should have mouseMove event', () => {
-    const wrapper = mountName()
-    const dot = wrapper.findComponent(Dot)
-    expect(dot.exists()).toBe(true)
+  describe('events', () => {
+    describe('mouse move', () => {
+      it('should change the dot\'s radius', async () => {
+        const wrapper = mountName()
+        const dot = wrapper.findComponent(Dot)
+        expect(dot.exists()).toBe(true)
 
-    wrapper.trigger('mousemove')
+        wrapper.trigger('mousemove', { offsetX: 100, offsetY: 100 })
 
-    // dot.trigger('mousemove')
+        await delay(150)
 
-    // expect(wrapper.emitted('mouseMove')).toBeTruthy()
-    wrapper.unmount()
+        expect(dot.classes()).toContain('dark:fill-white')
+
+        wrapper.unmount()
+      })
+    })
+
+    describe('mouse leave', () => {
+      it('should have mouseLeave event', () => {
+        const wrapper = mountName()
+        const dot = wrapper.findComponent(Dot)
+        expect(dot.exists()).toBe(true)
+
+        wrapper.trigger('mouseleave')
+        wrapper.unmount()
+      })
+    })
   })
 
   it('should be accessible', () => {
