2a02380bf942725034252604f2c8189ed2067fd9
Dennis Smuda <smuda.dennis@gmail.com>
2025-02-23 18:41:08 +0100

    test: fix delay for animation + init home widget spec (#69)

---
+++ widgets/home/Home.spec.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest'
+import Home from './Home.vue'
+
+describe('widget: home', () => {
+  it('should be defined', () => {
+    expect(Home).toBeDefined()
+  })
+})
