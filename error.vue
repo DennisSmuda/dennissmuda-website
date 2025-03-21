a8e1a7b62d8be40c321a285fa504b55fd695962e
Dennis Smuda <smuda.dennis@gmail.com>
2025-03-21 20:12:27 +0100

    feat: 404 page (#120)
    * chore: update lockfile
    
    * feat: add error page
    
    * feat: move error page
    
    * chore: container + seo data

---
+++ error.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps({
+  error: Object as () => NuxtError,
+})
+
+useSeoMeta({
+  title: '404 Page not found | Dennis Smuda',
+  ogTitle: '404 Page not found | Dennis Smuda',
+  description: 'This page does not exist. Maybe you mistyped the URL or the page was moved.',
+  ogDescription: 'This page does not exist. Maybe you mistyped the URL or the page was moved.',
+})
+</script>
+
+<template>
+  <SharedPageTitle
+    :title="error?.statusCode"
+    :subtitle="error?.statusMessage"
+  />
+
+  <div class="container mx-auto px-6 md:px-8 pt-16 md:pb-4">
+    <NuxtLink class="marker-link mt-4" to="/">
+      go back home 🏡
+    </NuxtLink>
+  </div>
+</template>
