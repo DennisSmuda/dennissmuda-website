885f7fffd4e7dc64ccb963621c28221b53e683c0
Dennis Smuda <smuda.dennis@gmail.com>
2025-03-21 20:10:09 +0100

    chore: container + seo data

--- error.vue
+++ error.vue
@@ -4,6 +4,13 @@ import type { NuxtError } from '#app'
 defineProps({
   error: Object as () => NuxtError,
 })
+
+useSeoMeta({
+  title: '404 Page not found | Dennis Smuda',
+  ogTitle: '404 Page not found | Dennis Smuda',
+  description: 'This page does not exist. Maybe you mistyped the URL or the page was moved.',
+  ogDescription: 'This page does not exist. Maybe you mistyped the URL or the page was moved.',
+})
 </script>
 
 <template>
@@ -11,7 +18,10 @@ defineProps({
     :title="error?.statusCode"
     :subtitle="error?.statusMessage"
   />
-  <NuxtLink class="marker-link mt-4 mx-auto" to="/">
-    go back home 🏡
-  </NuxtLink>
+
+  <div class="container mx-auto px-6 md:px-8 pt-16 md:pb-4">
+    <NuxtLink class="marker-link mt-4" to="/">
+      go back home 🏡
+    </NuxtLink>
+  </div>
 </template>
