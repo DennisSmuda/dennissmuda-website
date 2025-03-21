be68b8d4cf2042b97946e049d92b259aa2076b08
Dennis Smuda <smuda.dennis@gmail.com>
2025-03-21 20:00:58 +0100

    feat: add error page

---
+++ pages/error.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps({
+  error: Object as () => NuxtError,
+})
+</script>
+
+<template>
+  <div>
+    <h1>{{ error?.statusCode }}</h1>
+    <NuxtLink to="/">
+      Go back home
+    </NuxtLink>
+  </div>
+</template>
