b1642a143fb4160e3f91d7e5db1c53e586833972
Dennis Smuda <smuda.dennis@gmail.com>
2025-03-21 20:07:02 +0100

    feat: move error page

---
+++ error.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps({
+  error: Object as () => NuxtError,
+})
+</script>
+
+<template>
+  <SharedPageTitle
+    :title="error?.statusCode"
+    :subtitle="error?.statusMessage"
+  />
+  <NuxtLink class="marker-link mt-4 mx-auto" to="/">
+    go back home 🏡
+  </NuxtLink>
+</template>
